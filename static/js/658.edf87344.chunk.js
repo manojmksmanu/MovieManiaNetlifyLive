4431a94a25d8feb8bdfe5c35d9da6096b8b786c7
manojmksmanu <manoj2022019@gmail.com>
2023-09-10 12:49:33 +0530

    file upload

---
+++ static/js/658.edf87344.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmoviemania=self.webpackChunkmoviemania||[]).push([[658],{6658:function(n,e,a){a.r(e);var o=a(8870),r=a(2165),i=(a(6497),a(6593),a(4277),a(3328),a(4633)),t=a(184);e.default=function(n){var e=n.data;return(0,t.jsx)(r.tq,{modules:[o.W_,o.tl,o.LW,o.s5],navigation:!0,scrollbar:{draggable:!0},onSwiper:function(n){return console.log(n)},onSlideChange:function(){return console.log("slide change")},children:e&&e.map((function(n){return(0,t.jsx)(r.o5,{children:(0,t.jsx)(i.Z,{data:n})})}))})}}}]);
+//# sourceMappingURL=658.edf87344.chunk.js.map
